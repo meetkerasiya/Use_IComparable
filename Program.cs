a6a327707d2e3c2878014b88d5c4764ee4fd0750
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-18 11:31:47 +0530

    Generic IComparable added with new ToCompare methods

--- Program.cs
+++ Program.cs
@@ -16,10 +16,63 @@
             {
                 Console.WriteLine(student);
             }
+            Console.WriteLine();
+            var authors = new Author[5];
+            for (int i =0;i<authors.Length;i++)
+            {
+                authors[i]= new Author(students[i].FirstName, students[i].LastName);
+            }
+            Array.Sort(authors);
+            foreach(var author in authors)
+            {
+                Console.WriteLine(author);
+            }
         }
 
     }
-    public class Student : IComparable
+
+   
+    public class Author : IComparable<Author>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Author(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+       /* public int CompareTo(object? obj)
+        {
+            if (obj == null) return 1;
+            if (obj is Author someAuthor)
+            {
+                if (someAuthor.FirstName == this.FirstName)
+                {
+                    return this.LastName.CompareTo(someAuthor.LastName);
+                }
+                return this.FirstName.CompareTo(someAuthor.FirstName);
+            }
+            throw new ArgumentException("Not a Author", nameof(obj));
+        }
+       */
+        public int CompareTo(Author? other)
+        {
+            if (!(other is Author)) return 1;
+            if (other.FirstName == this.FirstName)
+            {
+                return this.LastName.CompareTo(other.LastName);
+                
+            }
+            return this.FirstName.CompareTo(other.FirstName);
+        }
+        
+    }
+    public class Student : IComparable<Student>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -33,6 +86,7 @@
         {
             return $"{FirstName} {LastName}";
         }
+        /*
         public int CompareTo(object? obj)
         {
             if(obj == null) return 1 ;
@@ -46,6 +100,15 @@
             }
             throw new ArgumentException("Not a Student", nameof(obj));
         }
-     
+        */
+        public int CompareTo(Student? other)
+        {
+            if(!(other is Student)) return 1 ;  
+            if(other.LastName==this.LastName)
+            {
+                return this.FirstName.CompareTo(other.FirstName);
+            }
+            return this.LastName.CompareTo(other.LastName);
+        }
     }
 }
